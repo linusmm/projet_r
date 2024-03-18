93844e97eee70dac2376da6e3338e8b88167111c
sandrinevrignon <sandrinevrignon34@gmail.com>
2024-03-18 20:55:50 +0100

    Création des images sur ggplot

--- picross.R
+++ picross.R
@@ -1,9 +1,39 @@
 #Librairies à utiliser
-library(ggplot2)
+library(tidyverse)
 
 #creation picross 5X5
-p5X5<-readxl::read_xlsx("picross_5X5.xlsx",sheet = "snake",col_names = F)
+p5X5<-readxl::read_xlsx("picross_5X5.xlsx",sheet = "snake",col_names = F) %>%
+  mutate(y=c(1:nrow(.))) %>%
+  pivot_longer(cols=1:ncol(.)-1,names_to = "x",values_to = "couleur")%>%
+  mutate(x=as.numeric(paste(substr(x,4,4)))*-1)
 p5X5
-ggplot(p5X5,aes(x=V1,y=1)) +
-  geom_point(color=1,size=4,shape=15)
+
+
+ggplot(p5X5,aes(x,y,colour=couleur,shape=as.factor(couleur)))+geom_point(size=29)+scale_shape_manual(values=c(15,15))
+
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("Matrice de Boutons avec Shiny"),
+  uiOutput("matrice_boutons")
+)
+
+server <- function(input, output) {
+  output$matrice_boutons <- renderUI({
+    nRows <- 5    # Définir le nombre de lignes
+    nCols <- 5    # Définir le nombre de colonnes
+
+
+    # Créer une matrice de boutons
+    boutons <- lapply(1:(nRows * nCols), function(i) {
+      actionButton(inputId = paste("bouton", i), label = paste("bouton", i),width = 5)
+    })
+
+    # Organiser les boutons en grille
+    tagList(div(class = "btn-grid",
+                lapply(split(boutons, ceiling(seq_along(boutons) / nCols)), div, class = "button-row")))
+  })
+}
+
+shinyApp(ui = ui, server = server)
 
