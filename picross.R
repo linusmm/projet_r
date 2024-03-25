e6cc93e3701f09cec379bb07d961ab3a8204e053
sandrinevrignon <sandrinevrignon34@gmail.com>
2024-03-25 22:03:05 +0100

    Génération d'un comptage des carrées noirs sur le picross. Essai mise d'une image Intégration d'un tableau clique bouton

--- picross.R
+++ picross.R
@@ -1,56 +1,37 @@
-#Librairies à utiliser
+#Librairie
+library("stringi")
+library(shiny)
 library(tidyverse)
 
-#creation picross 5X5
+#creation picross 5X5 d'essai
 p5X5<-readxl::read_xlsx("picross_5X5.xlsx",sheet = "snake",col_names = F)
 
-
-library("stringi")
-
+#Comptage du nombre de carreaux noire qui se suivent
 vec<- paste(p5X5$...1,collapse="")
 attributes(gregexpr("1+", vec)[[1]])[["match.length"]]
 
 lapply(p5X5, function(i){attributes(gregexpr("1+", p5X5)[[1]])[["match.length"]]})
 
-
-
+#Conversion pour générer le ggplot
 p5X5<-readxl::read_xlsx("picross_5X5.xlsx",sheet = "snake",col_names = F) %>%
   mutate(y=c(1:nrow(.))) %>%
   pivot_longer(cols=1:ncol(.)-1,names_to = "x",values_to = "couleur")%>%
   mutate(x=as.numeric(paste(substr(x,4,4)))*-1,
          couleur=as.factor(couleur))
 p5X5
-niveau<-readxl::read_xlsx("Choix niveau.xlsx",col_names = T)
 
 ggplot(p5X5,aes(x,y,colour=couleur,shape=couleur))+geom_point(size=29)+scale_shape_manual(values=c(15,15))+scale_colour_manual(values=c("white","black"))
 
-#Connexion au serveur shiny
-library(shiny)
-library(tidyverse)
-library(png)
-  #Page d'entrée de bienvenu
-p5X5<-readxl::read_xlsx("picross_5X5.xlsx",sheet = "snake",col_names = F) %>%
-  mutate(y=c(1:nrow(.))) %>%
-  pivot_longer(cols=1:ncol(.)-1,names_to = "x",values_to = "couleur")%>%
-  mutate(x=as.numeric(paste(substr(x,4,4)))*-1,
-         couleur=as.factor(couleur))
-p5X5
-choix<-readxl::read_xlsx("Choix niveau.xlsx")
-essai<-readPNG("image.png")
-essai
-
-library('magick')
-
-img <- magick::image_read('./image.png')
+#Application shiny
 
 ui<-fluidPage(
               #Rajouter une image, icône amusante sur chaque bouton?
               #plotOutput("plot",choices= p5X5),
               #imageOutput("image",width="100%",choices=img),
-              titlePanel("Bienvenu dans le merveilleux monde de la relaxation et du casse tête"),
               #mainPanel(img(src='image.png', height = '100px', width = '100px')),
+              titlePanel("Bienvenu dans le merveilleux monde de la relaxation et du casse tête"),
               uiOutput("matrice_boutons"),
-              selectInput(inputId = "niveau", label="niveau",choices = unique(choix$Niveau)),
+              #selectInput(inputId = "niveau", label="niveau",choices = unique(choix$Niveau)),
               actionButton(inputId = "Facile", label ="Facile",choices=p5X5),#voir en remplaçant par un plotOutput() à l'intérieur
               actionButton(inputId = "Moyen", label ="moyen"),
               actionButton(inputId = "Difficile", label ="Difficile"),
@@ -67,7 +48,7 @@ server<-function(input,output,session){
     nCols <- 5    # Définir le nombre de colonnes
     # Créer une matrice de boutons
     boutons <- lapply(1:(nRows * nCols), function(i) {
-      actionButton(inputId = paste("bouton", i), label = paste("X", i),width = 5, icon = icon("square"))
+      actionButton(inputId = paste("bouton", i), label = paste("X", i),width = 5, icon = icon("square", class="fa-sharp fa-solid fa-square"))
     })
     # Organiser les boutons en grille
     tagList(div(class = "btn-grid",
